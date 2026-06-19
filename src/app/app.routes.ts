8907cc6a61a6dc6476fe45adafd8fbf7b1dc94e7
Andrew Bargan <andrew.bargan@gmail.com>
2026-06-19 14:38:30 +0300

    fix: redirect to main page when unknown path requested

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,7 @@
 import { Routes } from '@angular/router';
 import { BooksComponent } from './pages/books/books.component';
 
-export const routes: Routes = [{ path: '', component: BooksComponent }];
+export const routes: Routes = [
+  { path: '', component: BooksComponent },
+  { path: '**', redirectTo: '/' },
+];
